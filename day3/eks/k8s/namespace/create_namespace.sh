f387a09ad5e486f5ebe636d258566d09317d8d4f
root <root@ip-172-16-1-96.ec2.internal>
2021-04-18 16:06:06 +0000

    update k8s

--- day3/eks/k8s/namespace/create_namespace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SET=$(seq 1 29)
-
-for i in $SET
-do
-  kubectl create namespace sk-user-$i-ns
-done
--- day3/eks/k8s/namespace/delete_namespace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SET=$(seq 1 29)
-
-for i in $SET
-do
-  kubectl delete namespace sk-user-$i-ns
-done
